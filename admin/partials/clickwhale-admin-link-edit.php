442148e7eee6cfc3a9a0fdfc69411d2530245d9c
peterkrupenya <62963646+peterkrupenya@users.noreply.github.com>
2022-05-27 16:41:05 +0300

    fix notice Undefined index: nonce

--- admin/partials/clickwhale-admin-link-edit.php
+++ admin/partials/clickwhale-admin-link-edit.php
@@ -22,7 +22,7 @@ $link_edit = new Clickwhale_Link_Edit();
 $link_categories = $link_edit->get_link_categories();
 
 // here we are verifying does this request is post back and have correct nonce
-if (wp_verify_nonce($_REQUEST['nonce'], basename(__FILE__))) {
+if (isset($_REQUEST['nonce']) && wp_verify_nonce($_REQUEST['nonce'], basename(__FILE__))) {
     // combine our default item with request params
     $item = shortcode_atts($default, $_REQUEST);
     // validate data, and if all ok save item to database
