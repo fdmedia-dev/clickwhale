4541e67f67dabba87ae7fce8f9d8cf9a6119a241
peterkrupenya <62963646+peterkrupenya@users.noreply.github.com>
2022-05-31 15:25:07 +0300

    edit gitignore, add vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '81347da62ad1be48755275833658411ab58d4a55',
+        'name' => 'clickwhale/clickwhale',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'clickwhale/clickwhale' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '81347da62ad1be48755275833658411ab58d4a55',
+            'dev_requirement' => false,
+        ),
+        'psr/cache' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/cache',
+            'aliases' => array(),
+            'reference' => 'd11b50ad223250cf17b86e38383413f5a6764bf8',
+            'dev_requirement' => false,
+        ),
+        'whichbrowser/parser' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../whichbrowser/parser',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '1044880bc792dbce5948fbff22ae731c43c280d9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
