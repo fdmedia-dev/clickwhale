8819383e6742396e40c39e9b67145a669f76c37c
petrokrupenia <info@krapan.net>
2022-07-25 17:46:38 +0300

    sonarcloud fixes

--- admin/settings/class-clickwhale-linkpages-list-table.php
+++ admin/settings/class-clickwhale-linkpages-list-table.php
@@ -6,21 +6,17 @@
  */
 class ClickwhaleLinkpagesListTable extends WP_List_Table {
 
-	private $users_data;
-
 	function __construct() {
 		global $status, $page;
 		parent::__construct(
 			array(
-				'singular' => 'link',
-				'plural'   => 'links',
+				'singular' => 'linkpage',
+				'plural'   => 'linkpages',
 			)
 		);
 	}
 
 	/**
-	 * [REQUIRED] this is a default column renderer
-	 *
 	 * @param $item - row (key, value array)
 	 * @param $column_name - string (key)
 	 *
@@ -31,27 +27,17 @@ class ClickwhaleLinkpagesListTable extends WP_List_Table {
 	}
 
 	/**
-	 * Render columns
-	 * method name must be like this: "column_[column_name]"
-	 *
 	 * @param $item - row (key, value array)
 	 *
 	 * @return HTML
 	 */
 
 	/**
-	 * Render column with actions,
-	 * when you hover row "Edit | Delete" links showed
-	 *
 	 * @param $item - row (key, value array)
 	 *
 	 * @return string
 	 */
 	function column_title( $item ) {
-		// links going to /admin.php?page=[your_plugin_page][&other_params]
-		// notice how we used $_REQUEST['page'], so action will be done on curren page
-		// also notice how we use $this->_args['singular'] so in this example it will
-		// be something like &link=2
 		$title   = sprintf( '<a href="?page=clickwhale-edit-linkpage&id=%s">%s</a>', $item['id'], $item['title'] );
 		$actions = array(
 			'edit'   => sprintf( '<a href="?page=clickwhale-edit-linkpage&id=%s">%s</a>', $item['id'], __( 'Edit', 'clickwhale' ) ),
@@ -66,8 +52,6 @@ class ClickwhaleLinkpagesListTable extends WP_List_Table {
 	}
 
 	/**
-	 * Link url with copy button
-	 *
 	 * @param $item - row (key, value array)
 	 *
 	 * @return string
@@ -79,8 +63,6 @@ class ClickwhaleLinkpagesListTable extends WP_List_Table {
 	}
 
 	/**
-	 * Total views per linkpage
-	 *
 	 * @param $item - row (key, value array)
 	 *
 	 * @return string
@@ -93,8 +75,6 @@ class ClickwhaleLinkpagesListTable extends WP_List_Table {
 
 
 	/**
-	 * [REQUIRED] this is how checkbox column renders
-	 *
 	 * @param $item - row (key, value array)
 	 *
 	 * @return string
@@ -107,10 +87,6 @@ class ClickwhaleLinkpagesListTable extends WP_List_Table {
 	}
 
 	/**
-	 * [REQUIRED] This method return columns to display in table
-	 * you can skip columns that you do not want to show
-	 * like content, or description
-	 *
 	 * @return array
 	 */
 	function get_columns() {
@@ -123,10 +99,6 @@ class ClickwhaleLinkpagesListTable extends WP_List_Table {
 	}
 
 	/**
-	 * [OPTIONAL] This method return columns that may be used to sort table
-	 * all strings in array - is column names
-	 * notice that true on name column means that its default sort
-	 *
 	 * @return array
 	 */
 	function get_sortable_columns() {
@@ -136,8 +108,6 @@ class ClickwhaleLinkpagesListTable extends WP_List_Table {
 	}
 
 	/**
-	 * Return array of bult actions if has any
-	 *
 	 * @return array
 	 */
 	function get_bulk_actions() {
@@ -146,13 +116,6 @@ class ClickwhaleLinkpagesListTable extends WP_List_Table {
 		);
 	}
 
-	/**
-	 * This method processes bulk actions
-	 * it can be outside of class
-	 * it can not use wp_redirect coz there is output already
-	 * in this example we are processing delete action
-	 * message about successful deletion will be shown on page in next part
-	 */
 	function process_bulk_action() {
 		global $wpdb;
 
@@ -177,45 +140,29 @@ class ClickwhaleLinkpagesListTable extends WP_List_Table {
 		}
 	}
 
-	/**
-	 * This is the most important method
-	 *
-	 * It will get rows from database and prepare them to be showed in table
-	 */
 	function prepare_items() {
 		global $wpdb;
 
-		$per_page     = 20; // constant, how much records will be shown per page
-		$current_page = $this->get_pagenum();
-		$columns      = $this->get_columns();
-		$hidden       = array();
-		$sortable     = $this->get_sortable_columns();
+		$per_page    = 20;
+		$columns     = $this->get_columns();
+		$hidden      = [];
+		$sortable    = $this->get_sortable_columns();
+		$total_items = $wpdb->get_var( "SELECT COUNT(id) FROM {$wpdb->prefix}clickwhale_linkpages" );
 
-		// here we configure table headers, defined in our methods
 		$this->_column_headers = array( $columns, $hidden, $sortable );
-
-		//  process bulk action if any
 		$this->process_bulk_action();
 
-		// will be used in pagination settings
-		$this->users_data = $this->get_users_data();
-		$total_items      = $wpdb->get_var( "SELECT COUNT(id) FROM {$wpdb->prefix}clickwhale_linkpages" );
-
-		// prepare query params, as usual current page, order by and order direction
 		$paged   = isset( $_REQUEST['paged'] ) ? ( $per_page * max( 0, intval( $_REQUEST['paged'] ) - 1 ) ) : 0;
 		$orderby = ( isset( $_REQUEST['orderby'] ) && in_array( $_REQUEST['orderby'], array_keys( $this->get_sortable_columns() ) ) ) ? sanitize_text_field( 'title' ) : 'id';
 		if ( isset( $_REQUEST['order'] ) ) {
 			$order = $_REQUEST['order'] === 'asc' ? 'asc' : 'desc';
 		}
-		// [REQUIRED] define $items array
-		// notice that last argument is ARRAY_A, so we will retrieve array
 		$this->items = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}clickwhale_linkpages ORDER BY $orderby $order LIMIT %d OFFSET %d", $per_page, $paged ), ARRAY_A );
 
-		// [REQUIRED] configure pagination
 		$this->set_pagination_args( array(
-			'total_items' => $total_items,                  // total items defined above
-			'per_page'    => $per_page,                        // per page constant defined at top of method
-			'total_pages' => ceil( $total_items / $per_page ) // calculate pages count
+			'total_items' => $total_items,
+			'per_page'    => $per_page,
+			'total_pages' => ceil( $total_items / $per_page )
 		) );
 	}
 
