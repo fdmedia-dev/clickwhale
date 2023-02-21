4353ef0dfd84ac89b25986560f47722d7cb069a9
Petro Krupenia <petro.krupenia@gmail.com>
2023-02-21 12:41:55 +0200

    fix sonarclout issues https://github.com/fdmedia-io/clickwhale/issues/146

--- admin/helpers/class-clickwhale-links-helper.php
+++ admin/helpers/class-clickwhale-links-helper.php
@@ -11,12 +11,15 @@ class ClickwhaleLinksHelper {
 		return apply_filters( 'clickwhale_links_limit', 1 );
 	}
 
+	/**
+	 * @throws Exception
+	 */
 	public static function get_random_slug( int $length = 6 ): string {
 		$characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 		$string     = '';
 
 		for ( $i = 0; $i < $length; $i ++ ) {
-			$string .= $characters[ mt_rand( 0, strlen( $characters ) - 1 ) ];
+			$string .= $characters[ random_int( 0, strlen( $characters ) - 1 ) ];
 		}
 
 		return $string;
